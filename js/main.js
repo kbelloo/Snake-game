36c897f0098c4e1ebe3cc75621c2a042446615df
Khalid Bello <khalidbello279@gmail.com>
2021-01-01 01:50:41 +0100

    project complete

--- js/main.js
+++ js/main.js
@@ -6,6 +6,9 @@ let currentSnake = [2, 1, 0];
 let direction = 1;
 const width = 10;
 let appleIndex = 0;
+let scoreValue = 0;
+let intervalTime = 1000;
+let timeId = 0;
 
 
 
@@ -33,16 +36,44 @@ currentSnake.forEach(index => squares[index].classList.add('snake'));
 
 
 
+//start game
+
+function startGame() {
+
+    currentSnake.forEach(index => squares[index].classList.remove('snake'));
+    
+    squares[appleIndex].classList.remove('apple');
+    
+    clearInterval(timeId);
+    currentSnake = [2, 1, 0];
+    scoreValue = 0;
+    score.textContent = scoreValue;
+    direction = 1;
+    intervalTime = 1000;
+    genApples();
+    
+    currentSnake.forEach(index => squares[index].classList.add('snake'));
+    
+    timeId = setInterval(move, intervalTime);
+
+}
+
+
+
+
+
+
+
 //moving the snake 
 
 function move() {
-    
+
 
     if (
 
-        (currentSnake[0] + width >= width*width && direction === width) ||
+        (currentSnake[0] + width >= width * width && direction === width) ||
 
-        (currentSnake[0] % width === width-1 && direction === 1) ||
+        (currentSnake[0] % width === width - 1 && direction === 1) ||
 
         (currentSnake[0] % width === 0 && direction === -1) ||
 
@@ -51,40 +82,46 @@ function move() {
         squares[currentSnake[0] + direction].classList.contains('snake')
     )
 
-    return clearInterval(timeId);
-    
-    
+        return clearInterval(timeId);
+
+
 
     const tail = currentSnake.pop();
     squares[tail].classList.remove('snake');
 
     let head = currentSnake.unshift(currentSnake[0] + direction);
-    
-    
+
+
 
     //snake head getting the apple
 
     if (squares[currentSnake[0]].classList.contains('apple')) {
 
         squares[currentSnake[0]].classList.remove('apple');
-        
+
         squares[tail].classList.add('snake');
-        
+
         currentSnake.push(tail);
-        
+
         genApples();
+
+        scoreValue++;
+
+        score.textContent = scoreValue;
+
+        clearInterval(timeId)
+
+        intervalTime = intervalTime * 0.9
+
+        timeId = setInterval(move, intervalTime);
     }
-        
-        
 
 
     squares[currentSnake[0]].classList.add('snake');
 
 };
 
-move();
 
-let timeId = setInterval(move, 1000);
 
 
 
@@ -128,3 +165,5 @@ function control(e) {
 };
 
 document.addEventListener('keyup', control);
+
+actionButton.addEventListener('click', startGame);
